2abe96dacfdae8332c278d61d5b49b6e0fca4819
Ravi6547 <r80340047@gmail.com>
2024-12-27 16:01:53 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        s \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
          // \App\Models\Admin::factory()->create();
     }
 }
